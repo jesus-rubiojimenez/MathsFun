8fb2a8b67a1720464abd707981fe93fc89bedae2
Jesús Rubio <53397884+jesus-rubiojimenez@users.noreply.github.com>
2022-02-08 15:06:56 +0000

    Polished description

--- ncklog.py
+++ ncklog.py
@@ -1,26 +1,27 @@
 import numpy as np
 
-def ncklog(n, k):
+def ncklog(n, k):   
+    
+    # Dr Jesús Rubio
+    # University of Exeter
+    # J.Rubio-Jimenez@exeter.ac.uk
+    #
+    # Created: Jan 2022
+    # Last modified: Feb 2022
+    #
     # Given the integers n and k, with k in [n, 0),
     #
     #   ncklog(n,k)
     #
     # returns the natural logarithm of the binomial coefficient c(n, k), i.e., log[c(n, k)].
-
+    #
+    # Originally coded in MATLAB for
+    #
+    #   J. Rubio et al., Phys. Rev. Lett. 127, 190402 (2021) - arXiv:2011.13018
+    #   https://github.com/jesus-rubiojimenez/QuThermometry-global
+    
     coefficient = 0
     for x in range(k + 1, n + 1):
         coefficient += np.log(x) - np.log(x - k)
 
     return coefficient
-
-# Dr Jesús Rubio
-# University of Exeter
-# J.Rubio-Jimenez@exeter.ac.uk
-#
-# Created: Jan 2022
-# Last modified: --
-#
-# Originally coded in MATLAB for
-#
-#   J. Rubio et al., Phys. Rev. Lett. 127, 190402 (2021) - arXiv:2011.13018
-#   https://github.com/jesus-rubiojimenez/QuThermometry-global
